3c41f25b8da44e5e0f588168ad9b46da12b387e2
Victor Hugo <54605503+Vkin-7@users.noreply.github.com>
2020-08-27 21:13:58 -0300

    test

--- src/App.js
+++ src/App.js
@@ -13,40 +13,40 @@ function App() {
   const config = {
     iceServers: [
       {
-        urls: ['stun:stun01.sipphone.com',
-        'stun:stun.ekiga.net',
-        'stun:stun.fwdnet.net',
-        'stun:stun.ideasip.com',
-        'stun:stun.iptel.org',
-        'stun:stun.rixtelecom.se',
-        'stun:stun.schlund.de',
-        'stun:stun.l.google.com:19302',
-        'stun:stun1.l.google.com:19302',
-        'stun:stun2.l.google.com:19302',
-        'stun:stun3.l.google.com:19302',
-        'stun:stun4.l.google.com:19302',
-        'stun:stunserver.org',
-        'stun:stun.softjoys.com',
-        'stun:stun.voiparound.com',
-        'stun:stun.voipbuster.com',
-        'stun:stun.voipstunt.com',
-        'stun:stun.voxgratia.org',
-        'stun:stun.xten.com',
-        {
-            url: 'turn:numb.viagenie.ca',
-            credential: 'muazkh',
-            username: 'webrtc@live.com'
-        },
-        {
-            url: 'turn:192.158.29.39:3478?transport=udp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        },
-        {
-            url: 'turn:192.158.29.39:3478?transport=tcp',
-            credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
-            username: '28224511:1379330808'
-        }]
+        urls: 'stun:stun01.sipphone.com',
+        // 'stun:stun.ekiga.net',
+        // 'stun:stun.fwdnet.net',
+        // 'stun:stun.ideasip.com',
+        // 'stun:stun.iptel.org',
+        // 'stun:stun.rixtelecom.se',
+        // 'stun:stun.schlund.de',
+        // 'stun:stun.l.google.com:19302',
+        // 'stun:stun1.l.google.com:19302',
+        // 'stun:stun2.l.google.com:19302',
+        // 'stun:stun3.l.google.com:19302',
+        // 'stun:stun4.l.google.com:19302',
+        // 'stun:stunserver.org',
+        // 'stun:stun.softjoys.com',
+        // 'stun:stun.voiparound.com',
+        // 'stun:stun.voipbuster.com',
+        // 'stun:stun.voipstunt.com',
+        // 'stun:stun.voxgratia.org',
+        // 'stun:stun.xten.com',
+        // {
+        //     url: 'turn:numb.viagenie.ca',
+        //     credential: 'muazkh',
+        //     username: 'webrtc@live.com'
+        // },
+        // {
+        //     url: 'turn:192.158.29.39:3478?transport=udp',
+        //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        //     username: '28224511:1379330808'
+        // },
+        // {
+        //     url: 'turn:192.158.29.39:3478?transport=tcp',
+        //     credential: 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+        //     username: '28224511:1379330808'
+        // }]
       },
     ],
   };
