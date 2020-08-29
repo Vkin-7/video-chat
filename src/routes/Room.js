558c5b4a7b56f7f1e78da2a27140f9344488bd72
Victor Hugo <54605503+Vkin-7@users.noreply.github.com>
2020-08-29 18:33:38 -0300

    class-room

--- src/routes/Room.js
+++ src/routes/Room.js
@@ -45,7 +45,7 @@ const Room = (props) => {
     const roomID = props.match.params.roomID;
 
     useEffect(() => {
-        socketRef.current = io.connect("http://localhost:3333");
+        socketRef.current = io.connect("https://server-socket-io.herokuapp.com");
         navigator.mediaDevices.getUserMedia({ video: videoConstraints, audio: true }).then(stream => {
             userVideo.current.srcObject = stream;
             socketRef.current.emit("join room", roomID);
